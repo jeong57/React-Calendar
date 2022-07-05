cbd0122c6c96fd494d499a4d9e26fc13feda4dc8
jeong57 <wjddlsp0507@naver.com>
2022-07-05 20:49:31 +0900

    0705

--- toy-project/src/components/ToDoItem.js
+++ toy-project/src/components/ToDoItem.js
@@ -1,58 +1,64 @@
-import {useEffect, useState} from "react";
-function ToDoItem(props) {
-    console.log(props.data)
-    const dataKey = props.dataKey
+import {useEffect, useState, useRef} from "react";
+
+function ToDoItem({times, dataKey, data, setData}) {
+    const intId = useRef(0);
+    
     // value: 새로 입력한 todo 값
-    const [value, setValue] = useState('')
+    // {
+      //   20220705 : [
+        //   {key:2, todo : value, isFinished : false,},
+        //   {key:3, todo2: value, isFinished: false}
+        //  ]
+        // }
+        
+    const [value, setValue] = useState('');
+    // const [count, setCount] = useState(0);
+    
     const changeValue = e => setValue(e.target.value);
-    // console.log(props.dataKey)
-    const handleSubmit=()=>{ 
-      if (props.data[dataKey]) { 
-        props.data[dataKey].push(value)
-        } else {
-          props.data[dataKey] = [value]
-        }
-      console.log(props.data)
-        // setData(data)
+
+    const handleSubmit=(event)=>{ 
+      event.preventDefault();
+      intId.current += 1;
+
+      const obj = {
+        key : intId.current,
+        todo : value,
+        isFinished : false,
+      }
+
+      try { 
+        data[dataKey] = [
+          obj,
+          ...data[dataKey] 
+        ]
+      } catch {
+        setData({})
+        // const day = dataKey
+        // data.day = [obj]
+        data[dataKey] = [obj]
+        // [data.dataKey] = [obj]
+      }
+      // else {
+      //     // props.data[dataKey] = [value]
+      //   data.dataKey = [obj]
+      // }
+      setData(data)
+      localStorage.setItem("todos", JSON.stringify(data));
+      setValue('');
     }
 
-    return (
-      <div>
-        <li>{props.times[1]}월 {props.times[2]}일</li>
+  return (
+    <div>
+      <li>{times[1]}월 {times[2]}일</li>
+      <form onSubmit={event => handleSubmit(event)}>
         <input 
           value = {value}
           onChange={changeValue}
           type="text" 
           placeholder="할 일"
         />
-        <button onClick={handleSubmit}>저장하기</button>
-        {/* 주석 */}
-        <div>
-          {/* 
-            {timeList.map((times)=> {
-              return (
-                <div>
-                  <div key={times[2]} style={{border: '1px solid black', display: 'grid'}}>
-                    <li >{times[1]}월 {times[2]}일 </li>
-                    <h2>Todo List</h2>
-                    <div>
-                        <input 
-                        onChange={handleChangeState}
-                        value={state.todo}
-                        name="todo"
-                        placeholder="할 일"
-                        type="text"
-                        />
-                    </div>
-                    <div>
-                    <button onClick={handleSubmit}>저장하기</button>
-                    </div> 
-                    
-                  </div>
-                </div>
-              )
-            }} */}
-        </div>
+        <button onClick={event => handleSubmit(event)}>저장</button>
+      </form>
     </div>
   )
 };
--- toy-project/src/components/ToDoList.js
+++ toy-project/src/components/ToDoList.js
@@ -4,27 +4,19 @@ import ToDoItem from "./ToDoItem.js"
 function Todos({timeList}){
     // localStorage에서 데이터 가져오기
     // data = todos = {"20220630": {"todo": ['ss', 'b', 'c']},}
-    const [data, setData] = useState({})
+    const [data, setData] = useState({"dsaf":"1234"})
 
     useEffect(() => {
-      const data = localStorage.getItem("todos")
-    } , [])
-
-    // const handleSubmit = (key, value, data) => {
-    //   if (data.key) {
-    //     data.
-    //   }
-    // }
-
-    // const handleSubmit=()=>{
-    //     localStorage.setItem("key", JSON.stringify(state));
-    // };
-    // const handleChangeState = (e) => {
-    //     setState({
-    //       ...state,
-    //       [e.target.name]: e.target.value
-    //     });
-    //   };
+      console.log(data)
+      try {
+        setData(JSON.parse(localStorage.getItem("todos")))
+      } catch {
+      }
+    }, [])
+    
+    const changeData = (val) => {
+      setData(val)
+    }
 
 
     return (
@@ -32,7 +24,7 @@ function Todos({timeList}){
         { timeList.length > 0 ? <p>{timeList[0][0]}년</p> : null}
         {timeList.map((times)=> {
           const dataKey = String(times[0]) + String(times[1]) + String(times[2])
-          return <ToDoItem times={times} dataKey={dataKey} data={data} setData={setData}/>
+          return <ToDoItem times={times} dataKey={dataKey} data={data} setData={changeData}/>
           })
         }
       </div>
